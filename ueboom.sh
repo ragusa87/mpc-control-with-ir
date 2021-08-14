5b8269e1aa2542469e6a9ad6663d4d0d373794a0
Laurent Constantin <laurent.constantin@liip.ch>
2021-08-14 09:25:55 +0100

    fix

--- ueboom.sh
+++ ueboom.sh
@@ -2,11 +2,11 @@
 set -e
 cd ~
 
-if [ -z $UEBOOM_MAC ];then
+if [ -z $UEBOOM_MAC ]; then
  echo "You should set mac address via env UEBOOM_MAC"
  exit 1
 fi
-i
+
 echo "Connect bluetooth"
 ./a2dp.py -t 2 $UEBOOM_MAC
 
