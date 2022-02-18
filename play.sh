4ef0f2f4f2f7413feb1c168b75b65068b29b3780
Laurent Constantin <laurent.constantin@liip.ch>
2022-02-18 10:27:32 +0000

    Improvements and random mode

--- play.sh
+++ play.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 if [ "$1" = "" ]; then
@@ -6,6 +6,21 @@ if [ "$1" = "" ]; then
  exit 1
 fi
 
+if [ "$1" = "random" ]; then
+ NUMBER=${2:-5}
+ mpc clear
+
+while read -r i; do
+  echo $i
+  mpc search artist "$i" | grep local | mpc add;
+done <<< "`mpc list artist | shuf | head -n $NUMBER | tac | tr -s ' ' '+'`"
+ mpc play
+ exit 0
+fi
+
+
+name=`echo "$1" | tr -s ' ' '+'`
+echo "Search $name"
 mpc clear
-mpc search artist $1 | grep local | mpc add
+mpc search artist "$name" | grep local | mpc add
 mpc play
