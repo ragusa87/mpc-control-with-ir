cf44a69cbd6a03673c3986e5444522555e14f06b
Laurent Constantin <laurent.constantin@liip.ch>
2022-06-17 18:32:43 +0100

    Add support outside mopidy/spotify

--- play.sh
+++ play.sh
@@ -12,7 +12,8 @@ if [ "$1" = "random" ]; then
 
 while read -r i; do
   echo $i
-  mpc search artist "$i" | grep local | mpc add;
+  #mpc search artist "$i" | grep local | mpc add;
+  mpc search artist "$i" | grep -v spotify | mpc add;
 done <<< "`mpc list artist | shuf | head -n $NUMBER | tac | tr -s ' ' '+'`"
  mpc play
  exit 0
@@ -22,5 +23,5 @@ fi
 name=`echo "$1" | tr -s ' ' '+'`
 echo "Search $name"
 mpc clear
-mpc search artist "$name" | grep local | mpc add
+mpc search artist "$name" | grep -v spotify | mpc add
 mpc play
