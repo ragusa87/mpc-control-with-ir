047de928cdaaf88c29f4cd643dcdee1c8e6eaff3
Laurent Constantin <laurent.constantin@liip.ch>
2023-04-21 13:56:46 +0100

    Bluetooth.sh suport args to connect/disconnect

--- bluetooth.sh
+++ bluetooth.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e
 
+action=$1
+
+if [ -z $action ]; then
+ action="connect"
+fi
+
+if [[ "$action" != "disconnect" ]] && [[ "$action" != "connect" ]]; then
+  echo "Bad action $action => Default to connect"
+  action="connect"
+fi
+
+
 if [ -z $UEBOOM_MAC ]; then
  echo "You should set mac address via env UEBOOM_MAC"
  exit 1
 fi
 
-echo "Connect bluetooth"
-bluetoothctl connect $UEBOOM_MAC
+echo "bluetooth $action"
+bluetoothctl $action $UEBOOM_MAC
 
 # Fallback on failure to ~/a2dp.py
 if [[ $? -ne 0 ]] && [[ -f ~/a2dp.py ]]; then
